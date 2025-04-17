f22eeb8f59063aa29b5e76cb01ae6e4e38e4f25b
Koushik <koushik96shenoy@gmail.com>
2025-04-17 16:07:27 +0200

    Deleted lines
    Removed the data loader lines.

--- ExperimentalTest.py
+++ ExperimentalTest.py
@@ -5,12 +5,6 @@ import numpy as np
 # Setting up the Doyle-Fuller-Newman Model
 model = bat.equivalent_circuit.Thevenin(options={"number of rc elements": 1})
 
-# Creating the drive cycle
-Data_Loader = bat.DataLoader()
-drive_cycle = pd.read_csv(
-    f"{Data_Loader.get_data('US06.csv')}", comment="#", header=None
-).to_numpy()
-
 # Simulating the DFN Model
 sim = bat.Simulation(model)
 
